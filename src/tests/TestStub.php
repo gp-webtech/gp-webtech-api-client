3259624de09393e637888020b1a7b9589f58a68a
George van Engers <vanengers@gmail.com>
2023-10-20 17:44:12 +0200

    API generation on OPENAPI json specs. Build extended wrapper to ensure login and token exchange

---
+++ src/tests/TestStub.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Vanengers\GpWebtechApiPhpClient\tests;
+
+class TestStub
+{
+    public const WORKING_API_CODE = 'da199a3a2b3cbd6162bf9f2d2e4d478cf2772a7a47d6b5f28b1a90c75688a048';
+}
